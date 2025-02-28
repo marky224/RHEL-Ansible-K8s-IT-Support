9fb51f273b7a819fce89267a1cc4ef387bef90e2
Mark <Markymark0224@gmail.com>
2025-02-27 19:42:52 -0600

    Update download_control-server.sh

--- Remote Scripts/Deploy/download_control-server.sh
+++ Remote Scripts/Deploy/download_control-server.sh
@@ -1,5 +1,5 @@
 # Manually install on PC with internet access
-mkdir -p /root/rhel-ansible-k8s
+sudo mkdir -p /root/rhel-ansible-k8s
 cd /root/rhel-ansible-k8s
 curl -O https://raw.githubusercontent.com/marky224/RHEL-Ansible-K8s-IT-Support/main/deploy/control-server.sh
 mkdir scripts
