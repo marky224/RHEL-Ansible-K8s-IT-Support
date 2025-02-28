2225c52af9c54fba15437a673b2d7a700a62a681
Mark <Markymark0224@gmail.com>
2025-02-27 20:42:32 -0600

    Delete Remote Scripts/Deploy/install-control-server.sh

--- Remote Scripts/Deploy/install-control-server.sh
+++
@@ -1,4 +0,0 @@
-# Manually install on PC with internet access
-sudo mkdir -p /root/rhel-ansible-k8s
-sudo cd /root/rhel-ansible-k8s
-curl -O https://raw.githubusercontent.com/marky224/RHEL-Ansible-K8s-IT-Support/main/Remote%20Scripts/Deploy/control-server.sh
