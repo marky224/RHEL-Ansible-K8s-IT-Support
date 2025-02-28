2383c09e0212dd619b07b8611ff6dc7a16edebe0
Mark <Markymark0224@gmail.com>
2025-02-27 19:50:56 -0600

    Update download_control-server.sh

--- Remote Scripts/Deploy/download_control-server.sh
+++ Remote Scripts/Deploy/download_control-server.sh
@@ -2,8 +2,8 @@
 sudo mkdir -p /root/rhel-ansible-k8s
 sudo cd /root/rhel-ansible-k8s
 curl -O https://raw.githubusercontent.com/marky224/RHEL-Ansible-K8s-IT-Support/main/deploy/control-server.sh
-mkdir scripts
-cd scripts
+sudo mkdir scripts
+sudo cd scripts
 curl -O https://raw.githubusercontent.com/marky224/RHEL-Ansible-K8s-IT-Support/main/scripts/Dockerfile
 curl -O https://raw.githubusercontent.com/marky224/RHEL-Ansible-K8s-IT-Support/main/scripts/entrypoint.sh
 curl -O https://raw.githubusercontent.com/marky224/RHEL-Ansible-K8s-IT-Support/main/scripts/checkin_listener.py
