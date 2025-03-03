9ff08c835a73101145dec84b24f21e115d8f7943
Mark <Markymark0224@gmail.com>
2025-03-03 13:30:40 -0600

    Delete Remote Scripts/AD-Setup directory

--- Remote Scripts/AD-Setup/ADUtils.psm1
+++
@@ -1,7 +0,0 @@
-# Helper functions
-function Write-Log {
-    param ([string]$Message)
-    $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
-    Write-Output "[$timestamp] $Message" | Out-File -FilePath "C:\ADSetup.log" -Append
-}
-Export-ModuleMember -Function Write-Log
